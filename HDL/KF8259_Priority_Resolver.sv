91dd586b50bd8413b4d57663ee7d2574955a7a4a
kitune-san <19359213+kitune-san@users.noreply.github.com>
2021-04-14 21:23:22 +0900

    Add rotate functions.

--- HDL/KF8259_Priority_Resolver.sv
+++ HDL/KF8259_Priority_Resolver.sv
@@ -39,6 +39,34 @@ module KF8259_Priority_Resolver (
     logic   [7:0]   priority_mask;
     logic   [7:0]   rotated_interrupt;
 
+    function logic [7:0] rotate_right (input [7:0] source, input [2:0] rotate);
+        casez (rotate)
+            3'b000:  rotate_right = { source[0],   source[7:1] };
+            3'b001:  rotate_right = { source[1:0], source[7:2] };
+            3'b010:  rotate_right = { source[2:0], source[7:3] };
+            3'b011:  rotate_right = { source[3:0], source[7:4] };
+            3'b100:  rotate_right = { source[4:0], source[7:5] };
+            3'b101:  rotate_right = { source[5:0], source[7:6] };
+            3'b110:  rotate_right = { source[6:0], source[7]   };
+            3'b111:  rotate_right = source;
+            default: rotate_right = source;
+        endcase
+    endfunction
+
+    function logic [7:0] rotate_left (input [7:0] source, input [2:0] rotate);
+        casez (rotate)
+            3'b000:  rotate_left = { source[6:0], source[7]   };
+            3'b001:  rotate_left = { source[5:0], source[7:6] };
+            3'b010:  rotate_left = { source[4:0], source[7:5] };
+            3'b011:  rotate_left = { source[3:0], source[7:4] };
+            3'b100:  rotate_left = { source[2:0], source[7:3] };
+            3'b101:  rotate_left = { source[1:0], source[7:2] };
+            3'b110:  rotate_left = { source[0],   source[7:1] };
+            3'b111:  rotate_left = source;
+            default: rotate_left = source;
+        endcase
+    endfunction
+
     function logic [7:0] resolv_priority (input [7:0] request);
         if      (request[0] == 1'b1)    resolv_priority = 8'b00000001;
         else if (request[1] == 1'b1)    resolv_priority = 8'b00000010;
@@ -51,44 +79,12 @@ module KF8259_Priority_Resolver (
         else                            resolv_priority = 8'b00000000;
     endfunction
 
-    always_comb begin
-        casez (priority_rotate)
-            3'b000:  rotated_request = { masked_interrupt_request[0],   masked_interrupt_request[7:1] };
-            3'b001:  rotated_request = { masked_interrupt_request[1:0], masked_interrupt_request[7:2] };
-            3'b010:  rotated_request = { masked_interrupt_request[2:0], masked_interrupt_request[7:3] };
-            3'b011:  rotated_request = { masked_interrupt_request[3:0], masked_interrupt_request[7:4] };
-            3'b100:  rotated_request = { masked_interrupt_request[4:0], masked_interrupt_request[7:5] };
-            3'b101:  rotated_request = { masked_interrupt_request[5:0], masked_interrupt_request[7:6] };
-            3'b110:  rotated_request = { masked_interrupt_request[6:0], masked_interrupt_request[7]   };
-            3'b111:  rotated_request = masked_interrupt_request;
-            default: rotated_request = masked_interrupt_request;
-        endcase
-    end
+    assign rotated_request = rotate_right(masked_interrupt_request, priority_rotate);
+
+    assign rotated_highest_level_in_service = rotate_right(highest_level_in_service, priority_rotate);
 
     always_comb begin
-        casez (priority_rotate)
-            3'b000:  rotated_in_service = { masked_in_service[0],   masked_in_service[7:1] };
-            3'b001:  rotated_in_service = { masked_in_service[1:0], masked_in_service[7:2] };
-            3'b010:  rotated_in_service = { masked_in_service[2:0], masked_in_service[7:3] };
-            3'b011:  rotated_in_service = { masked_in_service[3:0], masked_in_service[7:4] };
-            3'b100:  rotated_in_service = { masked_in_service[4:0], masked_in_service[7:5] };
-            3'b101:  rotated_in_service = { masked_in_service[5:0], masked_in_service[7:6] };
-            3'b110:  rotated_in_service = { masked_in_service[6:0], masked_in_service[7]   };
-            3'b111:  rotated_in_service = masked_in_service;
-            default: rotated_in_service = masked_in_service;
-        endcase
-
-        casez (priority_rotate)
-            3'b000:  rotated_highest_level_in_service = { highest_level_in_service[0],   highest_level_in_service[7:1] };
-            3'b001:  rotated_highest_level_in_service = { highest_level_in_service[1:0], highest_level_in_service[7:2] };
-            3'b010:  rotated_highest_level_in_service = { highest_level_in_service[2:0], highest_level_in_service[7:3] };
-            3'b011:  rotated_highest_level_in_service = { highest_level_in_service[3:0], highest_level_in_service[7:4] };
-            3'b100:  rotated_highest_level_in_service = { highest_level_in_service[4:0], highest_level_in_service[7:5] };
-            3'b101:  rotated_highest_level_in_service = { highest_level_in_service[5:0], highest_level_in_service[7:6] };
-            3'b110:  rotated_highest_level_in_service = { highest_level_in_service[6:0], highest_level_in_service[7]   };
-            3'b111:  rotated_highest_level_in_service = highest_level_in_service;
-            default: rotated_highest_level_in_service = highest_level_in_service;
-        endcase
+        rotated_in_service = rotate_right(masked_in_service, priority_rotate);
 
         if (special_fully_nest_config == 1'b1)
             rotated_in_service = (rotated_in_service & ~rotated_highest_level_in_service)
@@ -109,17 +105,6 @@ module KF8259_Priority_Resolver (
 
     assign rotated_interrupt = resolv_priority(rotated_request) & priority_mask;
 
-    always_comb begin
-        casez (priority_rotate)
-            3'b000:  interrupt = { rotated_interrupt[6:0], rotated_interrupt[7]   };
-            3'b001:  interrupt = { rotated_interrupt[5:0], rotated_interrupt[7:6] };
-            3'b010:  interrupt = { rotated_interrupt[4:0], rotated_interrupt[7:5] };
-            3'b011:  interrupt = { rotated_interrupt[3:0], rotated_interrupt[7:4] };
-            3'b100:  interrupt = { rotated_interrupt[2:0], rotated_interrupt[7:3] };
-            3'b101:  interrupt = { rotated_interrupt[1:0], rotated_interrupt[7:2] };
-            3'b110:  interrupt = { rotated_interrupt[0],   rotated_interrupt[7:1] };
-            3'b111:  interrupt = rotated_interrupt;
-            default: interrupt = rotated_interrupt;
-        endcase
-    end
+    assign interrupt = rotate_left(rotated_interrupt, priority_rotate);
+
 endmodule
