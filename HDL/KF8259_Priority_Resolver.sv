766a339d1b33dcd00c037e9b866ce3a4d610a3d9
kitune-san <19359213+kitune-san@users.noreply.github.com>
2021-04-20 22:01:20 +0900

    Create common package and move functions from priority resolver.

--- HDL/KF8259_Priority_Resolver.sv
+++ HDL/KF8259_Priority_Resolver.sv
@@ -3,6 +3,8 @@
 //
 // Written by Kitune-san
 //
+`include "KF8259_Common_Package.svh"
+
 module KF8259_Priority_Resolver (
     // Inputs from control logic
     input   logic   [2:0]   priority_rotate,
@@ -18,7 +20,9 @@ module KF8259_Priority_Resolver (
     // Outputs
     output  logic   [7:0]   interrupt
 );
-
+    import KF8259_Common_Package::rotate_right;
+    import KF8259_Common_Package::rotate_left;
+    import KF8259_Common_Package::resolv_priority;
 
     //
     // Masked flags
@@ -38,46 +42,6 @@ module KF8259_Priority_Resolver (
     logic   [7:0]   rotated_highest_level_in_service;
     logic   [7:0]   priority_mask;
     logic   [7:0]   rotated_interrupt;
-
-    function logic [7:0] rotate_right (input [7:0] source, input [2:0] rotate);
-        casez (rotate)
-            3'b000:  rotate_right = { source[0],   source[7:1] };
-            3'b001:  rotate_right = { source[1:0], source[7:2] };
-            3'b010:  rotate_right = { source[2:0], source[7:3] };
-            3'b011:  rotate_right = { source[3:0], source[7:4] };
-            3'b100:  rotate_right = { source[4:0], source[7:5] };
-            3'b101:  rotate_right = { source[5:0], source[7:6] };
-            3'b110:  rotate_right = { source[6:0], source[7]   };
-            3'b111:  rotate_right = source;
-            default: rotate_right = source;
-        endcase
-    endfunction
-
-    function logic [7:0] rotate_left (input [7:0] source, input [2:0] rotate);
-        casez (rotate)
-            3'b000:  rotate_left = { source[6:0], source[7]   };
-            3'b001:  rotate_left = { source[5:0], source[7:6] };
-            3'b010:  rotate_left = { source[4:0], source[7:5] };
-            3'b011:  rotate_left = { source[3:0], source[7:4] };
-            3'b100:  rotate_left = { source[2:0], source[7:3] };
-            3'b101:  rotate_left = { source[1:0], source[7:2] };
-            3'b110:  rotate_left = { source[0],   source[7:1] };
-            3'b111:  rotate_left = source;
-            default: rotate_left = source;
-        endcase
-    endfunction
-
-    function logic [7:0] resolv_priority (input [7:0] request);
-        if      (request[0] == 1'b1)    resolv_priority = 8'b00000001;
-        else if (request[1] == 1'b1)    resolv_priority = 8'b00000010;
-        else if (request[2] == 1'b1)    resolv_priority = 8'b00000100;
-        else if (request[3] == 1'b1)    resolv_priority = 8'b00001000;
-        else if (request[4] == 1'b1)    resolv_priority = 8'b00010000;
-        else if (request[5] == 1'b1)    resolv_priority = 8'b00100000;
-        else if (request[6] == 1'b1)    resolv_priority = 8'b01000000;
-        else if (request[7] == 1'b1)    resolv_priority = 8'b10000000;
-        else                            resolv_priority = 8'b00000000;
-    endfunction
 
     assign rotated_request = rotate_right(masked_interrupt_request, priority_rotate);
 
